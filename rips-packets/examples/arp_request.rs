b68e5b5c45dbf9b9bbdd0fb897b54bd567f47316
Linus Färnstrand <faern@faern.net>
2017-07-05 23:55:04 +0200

    Add examples

---
+++ rips-packets/examples/arp_request.rs
@@ -0,0 +1,65 @@
+extern crate rips_packets;
+
+use rips_packets::arp::{MutArpPacket, operations};
+use rips_packets::ethernet::{MutEthernetPacket, ether_types};
+use rips_packets::macaddr::{BROADCAST_MAC, MacAddr};
+
+use std::net::Ipv4Addr;
+
+fn main() {
+    let src_mac = MacAddr([0x13, 0x37, 0xde, 0xad, 0xbe, 0xef]);
+    let src_ip = Ipv4Addr::new(192, 168, 0, 150);
+    let target_ip = Ipv4Addr::new(192, 168, 0, 1);
+
+    let mut buffer = [0; 14 + 28];
+    format_arp_request_frame(&mut buffer[..], src_mac, src_ip, target_ip)
+        .expect("Unable to format frame");
+
+    println!(
+        "This is what an arp request for: IPv4 {}\nfrom: IPv4 {} and MAC {} looks like:\n{}",
+        target_ip,
+        src_ip,
+        src_mac,
+        buffer
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<Vec<String>>()
+            .join(" "),
+    );
+}
+
+fn format_arp_request_frame(
+    buffer: &mut [u8],
+    src_mac: MacAddr,
+    src_ip: Ipv4Addr,
+    target_ip: Ipv4Addr,
+) -> Result<(), &'static str> {
+    static ERR_MSG: &str = "Too short buffer";
+
+    let mut ethernet_packet = MutEthernetPacket::new(buffer).ok_or(ERR_MSG)?;
+    format_broadcast_ethernet_arp(&mut ethernet_packet, src_mac);
+
+    let mut arp_packet = MutArpPacket::new(ethernet_packet.payload()).ok_or(ERR_MSG)?;
+    format_arp_request(&mut arp_packet, src_mac, src_ip, target_ip);
+    Ok(())
+}
+
+fn format_broadcast_ethernet_arp<'a>(packet: &mut MutEthernetPacket<'a>, src_mac: MacAddr) {
+    packet.set_destination(BROADCAST_MAC);
+    packet.set_source(src_mac);
+    packet.set_ether_type(ether_types::ARP);
+}
+
+fn format_arp_request<'a>(
+    packet: &mut MutArpPacket<'a>,
+    src_mac: MacAddr,
+    src_ip: Ipv4Addr,
+    target_ip: Ipv4Addr,
+) {
+    packet.set_ipv4_over_ethernet_values();
+    packet.set_operation(operations::REQUEST);
+    packet.set_sender_mac_addr(src_mac);
+    packet.set_sender_ip_addr(src_ip);
+    // packet.set_target_mac_addr(); // Is ignored in a request anyway
+    packet.set_target_ip_addr(target_ip);
+}
---
+++ rips-packets/examples/basic.rs
@@ -0,0 +1,33 @@
+extern crate rips_packets;
+
+use rips_packets::ethernet::{EthernetPacket, MutEthernetPacket, ether_types};
+use rips_packets::macaddr::MacAddr;
+
+fn main() {
+    // Allocate a byte buffer that represents the bytes in the Ethernet frame.
+    let mut buffer = [0; 14];
+
+    {
+        // Lend the buffer mutably to `MutEthernetPacket` so it can manipulate the
+        // header fields.
+        let mut ethernet_packet = MutEthernetPacket::new(&mut buffer[..])
+            .expect("Too short buffer");
+
+        // Use the setter methods to change the data in `buffer`
+        ethernet_packet.set_destination(MacAddr::broadcast());
+        ethernet_packet.set_source(MacAddr([0x01, 0x02, 0x03, 0x04, 0x05, 0x06]));
+        ethernet_packet.set_ether_type(ether_types::IPV4);
+
+        // When `ethernet_packet` goes out of scope, the mutable borrow of `buffer` ends
+        // and we can access the buffer again.
+    }
+
+    // Create an immutable representation of the ethernet frame based on the same
+    // buffer. Where a mutable `MutFooPacket` has setters `FooPacket` has the
+    // corresponding getters.
+    let pkg = EthernetPacket::new(&buffer[..]).expect("Too short buffer");
+
+    println!("Destination MAC: {}", pkg.destination());
+    println!("Source MAC: {}", pkg.source());
+    println!("EtherType: {:?}", pkg.ether_type());
+}
