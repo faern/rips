ececcd010bd830abfec0f00fe0c79d1f577c16aa
Linus Färnstrand <faern@faern.net>
2017-08-03 23:51:37 +0200

    Add FromStr impl for MacAddr

--- packets/src/ethernet/macaddr.rs
+++ packets/src/ethernet/macaddr.rs
@@ -1,13 +1,40 @@
 use std::fmt;
+use std::str::FromStr;
+use std::error::Error;
 
 /// The broadcast MAC address. Used to broadcast to the local network.
 pub static BROADCAST_MAC: MacAddr = MacAddr([0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct MacAddrLengthError;
+
+impl fmt::Display for MacAddrLengthError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.description().fmt(f)
+    }
+}
+
+impl Error for MacAddrLengthError {
+    fn description(&self) -> &str {
+        "Given data not six bytes long"
+    }
+}
 
 /// A MAC address. Six bytes representing a link layer network address.
 #[derive(Copy, Clone, Default, Eq, PartialEq, Hash)]
 pub struct MacAddr(pub [u8; 6]);
 
 impl MacAddr {
+    /// Constructs a `MacAddr` from a slice of bytes.
+    /// Will fail if the given slice is not 6 bytes long.
+    pub fn try_from_slice(slice: &[u8]) -> Result<MacAddr, MacAddrLengthError> {
+        if slice.len() == 6 {
+            Ok(Self::from_slice(slice))
+        } else {
+            Err(MacAddrLengthError)
+        }
+    }
+
     /// Constructs a `MacAddr` from a slice of bytes.
     /// # Panics
     /// This function panics if the given slice is not 6 bytes long.
@@ -44,6 +71,36 @@ impl AsRef<[u8; 6]> for MacAddr {
 impl From<[u8; 6]> for MacAddr {
     fn from(data: [u8; 6]) -> Self {
         MacAddr(data)
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct MacAddrParseError(String);
+
+impl fmt::Display for MacAddrParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid MAC string: {}", self.0)
+    }
+}
+
+impl Error for MacAddrParseError {
+    fn description(&self) -> &str {
+        "Invalid MAC address string"
+    }
+}
+
+impl FromStr for MacAddr {
+    type Err = MacAddrParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use std::num::ParseIntError;
+
+        let bytes: Result<Vec<u8>, ParseIntError> =
+            s.split(":").map(|s| u8::from_str_radix(s, 16)).collect();
+        match bytes {
+            Ok(ref bytes) if bytes.len() == 6 => Ok(Self::from_slice(&bytes)),
+            _ => Err(MacAddrParseError(s.to_owned())),
+        }
     }
 }
 
@@ -111,5 +168,11 @@ mod tests {
     fn display() {
         let mac = MacAddr([255, 6, 7, 8, 9, 10]);
         assert_eq!("ff:06:07:08:09:0a", mac.to_string());
+    }
+
+    #[test]
+    fn from_str() {
+        let result = MacAddr::from_str("01:02:ff:ac:13:37");
+        assert_eq!(result, Ok(MacAddr([0x01, 0x02, 0xff, 0xac, 0x13, 0x37])));
     }
 }
