e93f202f7d4825d8310c6f6480258d9c71368833
Linus Färnstrand <faern@faern.net>
2018-03-27 14:55:35 +0200

    Add documentation and extra functionality to Protocol

--- packets/src/ip.rs
+++ packets/src/ip.rs
@@ -1,10 +1,23 @@
+/// Represents the eight bit header field in IPv4/IPv6 that defines what protocol the payload has.
+/// See [this list] for the full definition.
+///
+/// [this list]: https://en.wikipedia.org/wiki/List_of_IP_protocol_numbers
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Protocol(pub u8);
 
 impl Protocol {
+    /// Returns the numeric representation of this protocol.
     #[inline]
     pub fn value(&self) -> u8 {
         self.0
+    }
+
+    pub fn is_unassigned(&self) -> bool {
+        self.0 >= 143 && self.0 <= 252
+    }
+
+    pub fn is_experimental(&self) -> bool {
+        self.0 >= 253 && self.0 <= 254
     }
 }
 
@@ -14,4 +27,5 @@ pub mod protocols {
     pub const ICMP: Protocol = Protocol(1);
     pub const TCP: Protocol = Protocol(6);
     pub const UDP: Protocol = Protocol(17);
+    pub const RESERVED: Protocol = Protocol(255);
 }
